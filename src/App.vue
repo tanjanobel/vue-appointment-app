b8024cd9e7ec0ad2c330328bfadec013639f1f44
Tanja Nobel <tanja@nobel.swiss>
2021-07-14 10:34:36 +0200

    Add CalendarEntry component

--- src/App.vue
+++ src/App.vue
@@ -2,62 +2,23 @@
   <div id="app">
 
     <CalendarWeek />
-
-    <div id="calendar-entry">
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-6 offset-3">
-            <div class="card">
-              <div class="card-header text-center bg-vue">
-                <h5>Neuer Termin für: <strong>Dienstag</strong></h5>
-              </div>
-              <div class="card-body">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Neuer Termin"
-                >
-                <div class="mt-3 text-center">
-                  <span
-                    v-for="(color, index) in ['primary', 'success', 'info', 'warning', 'danger']"
-                    :key="index"
-                    class="alert me-2 square"
-                    :class="['alert-' +color]"
-                    style="cursor: pointer;"
-                  >
-
-                  </span>
-                </div>
-                <br>
-                <button class="btn bg-vue2 w-100">Eintragen</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <CalendarEntry />
 
   </div>
 </template>
 
 <script>
 import CalendarWeek from "./components/CalendarWeek";
+import CalendarEntry from "./components/CalendarEntry";
 
 export default {
   name: 'App',
   components: {
-    CalendarWeek
+    CalendarWeek,
+    CalendarEntry
   }
 }
 </script>
 
 <style scoped>
-  .bg-vue2 {
-    background-color: rgb(65, 184, 131);
-  }
-
-  .square {
-    height: 40px;
-    width: 40px;
-  }
 </style>
---
+++ src/components/CalendarEntry.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-6 offset-3">
+        <div class="card">
+          <div class="card-header text-center bg-vue">
+            <h5>Neuer Termin für: <strong>Dienstag</strong></h5>
+          </div>
+          <div class="card-body">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Neuer Termin"
+            >
+            <div class="mt-3 text-center">
+              <span
+                v-for="(color, index) in ['primary', 'success', 'info', 'warning', 'danger']"
+                :key="index"
+                class="alert me-2 square"
+                :class="[eventColor === color ? getBorderColor: '', 'alert-' +color]"
+                style="cursor: pointer;"
+                @click="changeEventColor(color)"
+              >
+              </span>
+            </div>
+            <br>
+            <button class="btn bg-vue2 w-100">Eintragen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CalendarEntry',
+    data() {
+      return {
+        eventColor: 'primary'
+      }
+    },
+    computed: {
+      getBorderColor() {
+        return 'border border-' +this.eventColor;
+      }
+    },
+    methods: {
+      changeEventColor: function (color) {
+        this.eventColor = color;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .bg-vue2 {
+    background-color: rgb(65, 184, 131);
+  }
+
+  .square {
+    height: 40px;
+    width: 40px;
+  }
+</style>
