abaeebcbf1dedb5eb670de6c39af13139feb9b6c
Tanja Nobel <tanja@nobel.swiss>
2021-07-14 08:28:43 +0200

    Add week component

---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card-group">
+          <CalendarDay
+            v-for="day in state.calendarWeekData"
+            :key="day.id"
+            :day="day"
+          />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { store } from '../store';
+import CalendarDay from './CalendarDay';
+
+export default {
+  name: 'CalendarWeek',
+  data() {
+    return {
+      state: store.state
+    }
+  },
+  components: {
+    CalendarDay
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
