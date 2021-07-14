25a75c230857254e7a294718131c6c60444e47aa
Tanja Nobel <tanja@nobel.swiss>
2021-07-14 08:07:01 +0200

    Add dummy data

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const calendarWeekData = [
+    {
+        id: 1,
+        shortname: 'Mo',
+        fullname: 'Montag',
+        events: [
+            { title: 'Vue.js lernen', edit: false, color: 'success' },
+            { title: 'Üben, üben, üben', edit: false, color: 'info' }
+        ],
+        active: false
+    },
+    {
+        id: 2,
+        shortname: 'Di',
+        fullname: 'Dienstag',
+        events: [],
+        active: false
+    },
+    {
+        id: 3,
+        shortname: 'Mi',
+        fullname: 'Mittwoch',
+        events: [],
+        active: true
+    },
+    {
+        id: 4,
+        shortname: 'Do',
+        fullname: 'Donnerstag',
+        events: [
+            { title: 'Grillen mit Freunden', edit: false, color: 'primary' }
+        ],
+        active: false
+    },
+    {
+        id: 5,
+        shortname: 'Fr',
+        fullname: 'Freitag',
+        events: [],
+        active: false
+    },
+    {
+        id: 6,
+        shortname: 'Sa',
+        fullname: 'Samstag',
+        events: [],
+        active: false
+    },
+    {
+        id: 7,
+        shortname: 'So',
+        fullname: 'Sonntag',
+        events: [
+            { title: 'Müll rausbringen', edit: false, color: 'danger' }
+        ],
+        active: false
+    }
+]
